3088f87133656b827dec61d5d7f5310ce6d6fdd5
Farhan Hussain <farhan03860@gmail.com>
2025-05-07 09:22:02 +0530

    Add files via upload

---
+++ arthematic of two numbers.py
@@ -0,0 +1,14 @@
+a=first number 
+b=second number
+ans1=a+b
+print("addition of",a,"and",b,"is",ans1)
+ans2=a-b
+print("subtraction of",a,"and",b,"is",ans2)
+ans3=a*b
+print("multiplication of",a,"and",b,"is",ans3)
+ans4=a/b
+print("division of",a,"and",b,"is",ans4)
+ans5=a%b
+print("modulus of",a,"and",b,"is",ans5)
+ans6=a//b
+print("floor division",a,"and",b,"is",ans6 )
